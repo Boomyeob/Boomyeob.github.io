c954423a248cc560c45c1bec61b59f482475be2c
Boom <65594327+Boomyeob@users.noreply.github.com>
2022-04-21 12:05:37 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,44 @@
+/*eslint-disable*/
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  let [글제목, 글제목변경] = useState([
+    "남자 코트 추천",
+    "강남 우동 맛집",
+    "파이썬독학",
+  ]);
+  let[따봉, 따봉변경]= useState(0);
+
+  let posts = "강남 고기 맛집";
+  
+function 제목변경 () {
+  []
+}
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <div>개발 blog</div>
+      </div>
+      <button onClick={ 제목변경}>버튼</button>
+      <div className="list">
+        <h3>{글제목[0]} <span onClick={ ()=>{따봉변경(따봉 + 1)} }>👍</span>{따봉}</h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3>{글제목[1]}</h3>
+        <p>2월 18일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3>{글제목[2]}</h3>
+        <p>2월 19일 발행</p>
+        <hr />
+      </div>
+    </div>
+  );
+}
+
+export default App;
